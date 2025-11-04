0031409a54ece385affe34eae12d3d1c2e2ba4d8
ivan629 <igolovac629@gmail.com>
2025-11-04 20:47:05 +0100

    feat: initial beta release

--- release.config.js
+++ release.config.js
@@ -3,8 +3,20 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
     '@semantic-release/npm',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ]
 };
